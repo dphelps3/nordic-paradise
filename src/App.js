48ad29aaa5fe9f692a50a3eb47b5f25e6fdf6e98
dphelps3 <dphelps34@gmail.com>
2023-02-19 19:12:54 -0600

    Update App.js to include paths

--- src/App.js
+++ src/App.js
@@ -2,49 +2,30 @@ import React, { Component } from "react"
 import logo from "./logo.svg"
 import "./App.css"
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import RecipesPage from './pages/RecipesPage';
+import BlogListPage from './pages/BlogListPage';
+import BlogPage from './pages/BlogPage';
+import NotFoundPage from './pages/NotFoundPage';
 
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+      <img src={logo} height={150} width={150} />
+      <div id="page-body">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/recipes" element={<RecipesPage />} />
+          <Route path="/blogs" element={<BlogListPage />} />
+          <Route path="/blogs/:blogId" element={<BlogPage />} />
+        </Routes>
       </div>
-    )
-  }
+    </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
+
